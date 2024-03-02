68231883e9dd921b2544fd5558c9253b3b273f25
abyankamal <kamalmuhammad414@gmail.com>
2024-03-02 16:19:36 +0700

    first commit

---
+++ started-project/app.js
@@ -0,0 +1,12 @@
+const buttonEl = document.querySelector("button");
+const inputEl = document.querySelector("input");
+const listEl = document.querySelector("ul");
+
+function addGoal() {
+  const listItemEl = document.createElement("li");
+  listItemEl.textContent = inputEl.value;
+  listEl.appendChild(listItemEl);
+  inputEl.value = "";
+}
+
+buttonEl.addEventListener("click", addGoal);
