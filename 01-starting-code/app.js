ac5c05abdc69ff38952740a2d3134d82e389c2ef
abyankamal <kamalmuhammad414@gmail.com>
2024-03-03 16:52:15 +0700

    basic vue

--- 01-starting-code/app.js
+++ 01-starting-code/app.js
@@ -2,7 +2,19 @@ Vue.createApp({
   data() {
     return {
       iseng: "Hello, world",
+      outputGoalA: "Apaan Dah",
+      outputGoalB: "Lu Sok Asik",
       link: "https://www.instagram.com/",
     };
+  },
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return this.outputGoalA;
+      } else {
+        return this.outputGoalB;
+      }
+    },
   },
 }).mount("#user-goal");
