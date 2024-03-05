8ccb65022219fa432f55a93d683ca957da71a27d
abyankamal <kamalmuhammad414@gmail.com>
2024-03-05 11:54:20 +0700

    add components

---
+++ 11-components-in-vue/app.js
@@ -0,0 +1,49 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      friends: [
+        {
+          id: "Manuel",
+          name: "Manuel Lorenz",
+          phone: "01234 5678 991",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "Julie",
+          name: "Julie Jones",
+          phone: "01234 5678 991",
+          email: "manuel@localhost.com",
+        },
+      ],
+    };
+  },
+});
+
+app.component("friend-contact", {
+  template: `<li>
+          <h2>{{friend.name}}</h2>
+          <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show'}} Details</button>
+          <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{friend.phone}}</li>
+            <li><strong>Email:</strong> {{friend.email}}</li>
+          </ul>
+        </li>`,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "Julie",
+        name: "Julie Jones",
+        phone: "01234 5678 991",
+        email: "manuel@localhost.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+});
+
+app.mount("#app");
