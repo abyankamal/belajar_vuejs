26653e7d5ced3ee9621222b0f9948a7c3890a400
abyankamal <kamalmuhammad414@gmail.com>
2024-03-08 18:03:01 +0700

    nambah http end

--- 18-vue-http/src/components/survey/UserExperiences.vue
+++ 18-vue-http/src/components/survey/UserExperiences.vue
@@ -3,9 +3,16 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperience"
+          >Load Submitted Experiences</base-button
+        >
       </div>
-      <ul>
+      <p v-if="isLoading">Loading...</p>
+      <p v-else-if="!isLoading && error">{{ error }}</p>
+      <p v-else-if="!isLoading && results.length === 0">
+        No Experiences Submitted
+      </p>
+      <ul v-else>
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -21,9 +28,50 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
+  // props: ['results'],
   components: {
     SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+      isLoading: false,
+      error: null,
+    };
+  },
+  methods: {
+    loadExperience() {
+      fetch(
+        'https://vue-http-demo-fdb2c-default-rtdb.firebaseio.com/surveys.json'
+      )
+        .then((response) => {
+          this.isLoading = true;
+          this.error = null;
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        .then((data) => {
+          this.isLoading = false;
+          const results = [];
+          for (const id in data) {
+            results.push({
+              id: id,
+              name: data[id].name,
+              rating: data[id].rating,
+            });
+          }
+          this.results = results;
+        })
+        .catch((error) => {
+          console.log(error);
+          this.isLoading = false;
+          this.error = 'Failed Tp Fetch Data';
+        });
+    },
+  },
+  mounted() {
+    this.loadExperience();
   },
 };
 </script>
