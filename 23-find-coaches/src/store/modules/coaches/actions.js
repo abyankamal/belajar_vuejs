deb626f2468961a73a908f7ee6ba1cb5131427f7
abyankamal <kamalmuhammad414@gmail.com>
2024-03-19 14:08:05 +0700

    nambah http error

--- 23-find-coaches/src/store/modules/coaches/actions.js
+++ 23-find-coaches/src/store/modules/coaches/actions.js
@@ -29,4 +29,31 @@ export default {
       id: userId,
     });
   },
+  async loadCoaches(context) {
+    const response = await fetch(
+      `https://vue-http-demo-fdb2c-default-rtdb.firebaseio.com/coaches/.json`
+    );
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(responseData.message || 'Failed To Fetch!');
+      throw error;
+    }
+
+    const coaches = [];
+
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas,
+      };
+      coaches.push(coach);
+    }
+    context.commit('setCoaches', coaches);
+  },
 };
